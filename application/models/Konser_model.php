218fef324ee3992417faa84e22c68cda3a2d608f
RanggaAnanda <rangga122005@gmail.com>
2025-10-21 21:12:13 +0700

    Update and rename konser_model.php to Konser_model.php

--- application/models/Konser_model.php
+++ application/models/Konser_model.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class konser_model extends CI_Model {
+class Konser_model extends CI_Model {
 
 	public function create()
     {
